a60918bf852c09052de70ab42bd20472d891267f
Bhairavi Kiran <bhairavitn1999@gmail.com>
2023-02-01 06:47:13 +0530

    files

---
+++ com/src/com/xwrkz/boot/tuesday/Earth.java
@@ -0,0 +1,39 @@
+package com.xwrkz.batch.tuesday;
+
+public class Earth {
+	String name;
+	double diameter;
+	String state;
+	String mainfactor;
+	int radius;
+String star;
+String satelliete;
+boolean living;
+String planet;
+String shape;
+String birth;
+String layers;
+int rotationtime;
+int revovletime;
+public void display()
+{
+	 System.out.println("name is:"+name);
+	 System.out.println("diameter is:"+diameter);
+
+	 System.out.println("state is:"+state);
+
+	 System.out.println("mainfactor is:"+mainfactor);
+	 System.out.println("radius is:"+radius);
+	 System.out.println("nearest star is:"+star);
+	 System.out.println(" natural satelliete is:"+satelliete);
+	 System.out.println("living is:"+living);
+	 System.out.println("planet is:"+planet);
+	 System.out.println("shape is:"+shape);
+	 System.out.println("birth is:"+birth);
+	 System.out.println("layers is:"+layers);
+	 System.out.println("rotationtime is:"+rotationtime);
+	 System.out.println("revovletime is:"+revovletime);
+
+
+}
+}
---
+++ com/src/com/xwrkz/boot/tuesday/Satellite.java
@@ -0,0 +1,40 @@
+package com.xwrkz.batch.tuesday;
+
+public class Satellite {
+	String name;
+	String material;
+	int capacity;
+	int seat;
+	String scientist;
+	String company;
+String type;
+int cost;
+String category;
+String distination;
+int manfyear;
+int lunchyear;
+boolean succesful;
+int lifespan;
+
+public void display()
+{
+	 System.out.println("name is:"+name);
+	 System.out.println("material is:"+material);
+	 System.out.println("capacity is:"+capacity);
+	 System.out.println("seat is:"+seat);
+	 System.out.println("scientist is:"+scientist);
+	 System.out.println("company is:"+company);
+	 System.out.println("type is:"+type);
+	 System.out.println("cost is:"+cost);
+	 System.out.println("category is:"+category);
+	 System.out.println("distination is:"+distination);
+	 System.out.println("manfyear is:"+manfyear);
+	 System.out.println("lunchyear is:"+lunchyear);
+	 System.out.println("succesful is:"+succesful);
+	 System.out.println("lifespan is:"+lifespan);
+
+	 
+
+
+}
+}
